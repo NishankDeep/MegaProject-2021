95434ef9cd04926d86e584de8ff9f0740fa0b555
Sid08JLUCKY <82439405+Sid08JLUCKY@users.noreply.github.com>
2021-12-27 22:35:22 +0530

    Add files via upload

---
+++ Rozgar/Scripts/home.js
@@ -0,0 +1,21 @@
+function myAccount1(){
+	document.getElementById('Account_list').style.display="inline-block"
+	document.getElementById('up1').style.display="inline-block"
+	document.getElementById('down1').style.display="none"
+}
+function myAccount2(){
+	document.getElementById('Account_list').style.display="none"
+	document.getElementById('up1').style.display="none"
+	document.getElementById('down1').style.display="inline-block"
+}
+
+function More1(){
+	document.getElementById('sp_list').style.display="inline-block"
+	document.getElementById('up2').style.display="inline-block"
+	document.getElementById('down2').style.display="none"
+}
+function More2(){
+	document.getElementById('sp_list').style.display="none"
+	document.getElementById('up2').style.display="none"
+	document.getElementById('down2').style.display="inline-block"
+}
